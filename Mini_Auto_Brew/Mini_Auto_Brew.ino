7734deba856254e5bbe922368667c5c8b95ccd3c
Alan <al.metivier@hotmail.fr>
2020-03-23 16:57:57 +0100

    Refonte de l'algo- Fonctionne!

--- Mini_Auto_Brew/Mini_Auto_Brew.ino
+++ Mini_Auto_Brew/Mini_Auto_Brew.ino
@@ -1,5 +1,3 @@
-#include <OneWire.h>
-
 /*
     AUTHOR : Alan Métivier : https://github.com/LesQuater/Mini_Auto_Brew.git
 */
@@ -10,36 +8,130 @@
 
 /* 8 Relay : 5v in command for a 220V AC 10A or 30 DC 10A
 
-           2 Valves
+           1 Valves
            1 pomp
-           1 electric hob
-           1 electric-resistance heated
+           2 electric-resistance heated
 */
 
-//  .. 220V AC Hob
-const int hob_PIN = 0;
-int hob_Value = 0;
 
-//  .. 220V AC Resistance (from electric kettle)
-const int kettle_PIN = 0;
-int kettle_Value = 0;
+  /*            
+   *    ---------
+   *   _|_      |
+   *            |
+   * |\Malt/|   |
+   * | \  / |   |      Cuve 1 : Sac malt
+   * |  \/  |   |
+   * |______|   |
+   *  | E  |    |      E : Electrovanne
+   * _|____|_   |
+   * |      |   |
+   * |  T   |-P--     Cuve 2 : - T : capteur de T°
+   * |S    S|                  - S : 2 thermo Resistance
+   * |______|                  - P : Pompe
+   * 
+   * PHASE 1 : <--------------------------------------
+   *  R : ON                                         |
+   *  Pompe : OFF                                    |
+   *  Vanne : ON                                     |
+   *  (L'eau est dans la cuve 2 et chauffe à 50°)    |
+   *                                                 |
+   *  ||Si|| T° = 50° ____                           |
+   *                      |                          |
+   *                      v                          | x 4 = 40 min
+   * PHASE 2 :                                       |
+   *  Pompe : ON                                     |
+   *  Vanne : OFF                                    |
+   *  R : OFF                                        |
+   *  (L'eau est dans la cuve 1 et noye le malt)     |
+   *                                                 |
+   *  ||Si|| Temps = 10 min et boucle =/= 4 ---------|
+   *  ||Sinon||  ____
+   *                 |
+   *                 v                 
+   * PHASE 3 : <--------------------------------------
+   *  R : ON                                         |
+   *  Pompe : OFF                                    |
+   *  Vanne : ON                                     |
+   *  (L'eau est dans la cuve 2 et chauffe à 60°)    |
+   *                                                 |
+   *  ||Si|| T° = 60° ____                           |
+   *                      |                          |
+   *                      v                          | x 2 = 20 min
+   * PHASE 4 :                                       |
+   *  Pompe : ON                                     |
+   *  Vanne : OFF                                    |
+   *  R : OFF                                        |
+   *  (L'eau est dans la cuve 1 et noye le malt)     |
+   *                                                 |
+   *  ||Si|| Temps = 10 min et boucle =/= 2 ---------|
+   *  ||Sinon||  ____
+   *                 |
+   *                 v
+   * PHASE 5 :
+   *  R : ON                                         
+   *  Pompe : OFF                                    
+   *  Vanne : ON                                     
+   *  (L'eau est dans la cuve 2 et chauffe à 70°)    
+   *                                                 
+   *  ||Si|| T° = 70° ____                           
+   *                      |                          
+   *                      v                          
+   * PHASE 6 :                                       
+   *  Pompe : ON                                     
+   *  Vanne : OFF                                    
+   *  R : OFF                                        
+   *  (L'eau est dans la cuve 1 et noye le malt)     
+   *                                                 
+   *  ||Si|| Temps = 10 min ____                           
+   *                            |                          
+   *                            v    
+   *    
+   *  PHASE 7 :
+   *   Vanne : ON
+   *   R : ON
+   *   Pompe : OFF
+   *   
+   *   ||Si|| Temps = 50 min ____
+   *                             |
+   *                             v
+   *  PHASE 8 :
+   *   Bip : ON
+   *   delay(1000)
+   *   Bip : OFF
+   *    
+   *   ||Si|| Temps = 10 min ___
+   *                             |
+   *                             v
+   *  PHASE 9 : 
+   *  Bip : ON
+   *  
+   *  FIN
+   * 
+  */
+
+//  .. 2x 220V AC Resistance
+const int heat1_PIN = 2;
+int heat1_Value = 0;
+const int heat2_PIN = 0;
+int heat2_Value = 0;
 
 //  .. 12 V DC Pomp for hot water Max ~ 8L/Min
-const int pump_PIN = 0;
+const int pump_PIN = 7;
 int pump_Value = 0;
 
-//  .. 12 V DC Solenoid valve ~ 1.3 l/min
-const int valve1_PIN = 0;
+//  .. 2x 12 V DC Solenoid valve ~ 1.3 l/min
+const int valve1_PIN = 5;
 int valve1_Value = 0;
-const int valve2_PIN = 0;
-int valve2_Value = 0;
 
-// Temperature sensor : DS18B20
-const byte T_PIN = 0;
-float T_Value[3];
+//Buzzer
+const int buz_PIn = 0;
+int buz_Value = 0;
+
+// 2x Temperature sensor : DS18B20
+const byte T_PIN = 10;
+float T_Value[2];
 float T1_Value = 0;
 float T2_Value = 0;
-float T3_Value = 0;
 
 enum DS18B20_RCODES {
   READ_OK,  // Lecture ok
@@ -54,98 +146,52 @@ OneWire ds(T_PIN);
 // LCD 20x4
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-// 3 Button
-const int X_PIN = A0;
-int X_Value = 0;
-const int Y_PIN = A1;
-int Y_Value = 0;
-const int enter_PIN = 2;
-int enter_Value = 0;
-int mapX = 0;
-int mapY = 0;
-
-//Buzzer?
-const int buz_PIn = 0;
-int buz_Value = 0;
-
-// Boolean
-boolean START;
-boolean STAGE1;
-boolean STAGE2;
-boolean STAGE3;
-boolean STAGE4;
-boolean STAGE5;
-boolean STAGE6;
-
-boolean level0;
-boolean level1;
-boolean level2;
-boolean level3;
-boolean level4;
-boolean level5;
-boolean level6;
+int PHASE;
 
 // Int
 int nbLiter;
 
-int startRunWater_Time;
-int washRunWater_Time;
-int addRunWater_Time;
-int heatGrain_Time;
-int heatWort_Time;
-int hop1_Time;
-int hop2_Time;
+int PHASE2_Time;
+int PHASE4_Time;
+int PHASE6_Time;
+int PHASE7_Time;
+int PHASE8_Time;
+
+int boucle1;
+int boucle2;
 
 
 void setup() {
   
   // OUTPUT  
-  pinMode(hob_PIN, OUTPUT);
-  
-  pinMode(kettle_PIN, OUTPUT);
+  pinMode(heat1_PIN, OUTPUT);
+  //pinMode(heat2_PIN, OUTPUT);
   
   pinMode(pump_PIN, OUTPUT);
   
   pinMode(valve1_PIN, OUTPUT);
-  pinMode(valve2_PIN, OUTPUT);
-  
-  pinMode(buz_PIn, OUTPUT);
-  
-  // INPUT  
-  pinMode(X_PIN,INPUT);
-  pinMode(Y_PIN,INPUT);
-  pinMode(enter_PIN,INPUT);
-  
-
-  
+    
+  //pinMode(buz_PIn, OUTPUT);
   
   // Initialize
-  digitalWrite(hob_PIN,LOW);
-  digitalWrite(kettle_PIN,LOW);
+  digitalWrite(heat1_PIN,LOW);
+  //digitalWrite(heat2_PIN,LOW);
+  
   digitalWrite(pump_PIN,LOW);
+  
   digitalWrite(valve1_PIN,LOW);
-  digitalWrite(valve2_PIN,LOW);
-  digitalWrite(buz_PIn,LOW);
   
-  START = true;
-  STAGE1 = false;
-  STAGE2 = false;
-  STAGE3 = false;
-  STAGE4 = false;
-  STAGE5 = false;
-  STAGE6 = false;
-  
-  level0 = true;
-  level1 = false;
-  level2 = false;
-  level3 = false;
-  level4 = false;
-  level5 = false;
-  level6 = false;
-  
-  nbLiter = 0;
-  heatGrain_Time = 0;
-  
+ // digitalWrite(buz_PIn,LOW);
+
+  boucle1 = 0;
+  boucle2 = 0;
+
+  PHASE = 0;
+  PHASE2_Time = 1000;
+  PHASE4_Time = 1000;
+  PHASE6_Time = 1000;
+  PHASE7_Time = 1000;
+  PHASE8_Time = 1000;
   
   //LCD Init
   lcd.init();
@@ -154,266 +200,195 @@ void setup() {
   lcd.setCursor(3,0);
   lcd.print("Mini_Auto_Brew");
   Serial.begin(9600);
-  delay(1000);
-  
+  delay(2000);
 }
 
 void loop() {
   
-  X_Value = analogRead(X_PIN);
-  Serial.println(X_Value);
-  Y_Value = digitalRead(Y_PIN);
-  Serial.println(Y_Value);
-  enter_Value = digitalRead(enter_PIN);
-  Serial.println(enter_Value);
-  mapX = map(X_Value, 0, 1023, -512, 512);
-  mapY = map(Y_Value, 0, 1023, -512, 512);
-  
-  /*if (getTemperature(&T_Value[0], true) != READ_OK) {
+  if (getTemperature(&T_Value[0], true) != READ_OK) {
     Serial.println(F("Erreur de lecture du capteur 1"));
     return;
   }
-  if (getTemperature(&T_Value[1], false) != READ_OK) {
-    Serial.println(F("Erreur de lecture du capteur 2"));
-    return;
-  }
-  if (getTemperature(&T_Value[2], false) != READ_OK) {
-    Serial.println(F("Erreur de lecture du capteur 3"));
-    return;
-  }*/
   
   T1_Value = T_Value[0];
-  T2_Value = T_Value[1];
-  T3_Value = T_Value[2];
+  Serial.println(T1_Value);
   
-  
-  
-  // Start menu : enter the recipe. (Button)
-  if(START)
-  {
-    
-    
-    if(enter_Value !=1 && level0)
-    {
-      lcd.clear();
-      lcd.setCursor(7,0);
-      lcd.print("START");
-      level1 = true;
-      level0 = false;
-    }
-    // LEVEL 1 : How many liter in fine
-    else if(level1)
-    { 
-      lcd.setCursor(0,1);
-      lcd.print("In liter, Max:5L");
-      lcd.setCursor(0,2);
-      lcd.print("H-M liter in fine ?");
-      lcd.setCursor(0,3);
-      lcd.print(nbLiter);
-      if(mapX > 1000 && nbLiter<5)
-      {
-        nbLiter ++;
-      }
-      else if(mapX < 10 && nbLiter>0)
-      {
-        nbLiter --;
-      }
-      else if(enter_Value !=1)
-      {
+  switch(PHASE){
+    case 0:
         lcd.clear();
         lcd.setCursor(7,0);
         lcd.print("START");
-        level2 = true;
-        level1 = false;
-      }
-      
-    }
-    // LEVEL 2 : How many time with the grain
-    else if(level2)
-    {
-      lcd.setCursor(0,1);
-      lcd.print("In minutes");
-      lcd.setCursor(0,2);
-      lcd.print("Time with the grain ?");
-      lcd.setCursor(0,3);
-      lcd.print(heatGrain_Time);
-      if(mapX > 1000)
-      {
-        heatGrain_Time ++;
-      }
-      else if(mapX < 10)
-      {
-        heatGrain_Time --;
-      }
-      else if(enter_Value !=1)
-      {
+        delay(10000);
+        PHASE = 1;
+      break;
+    case 1:
         lcd.clear();
         lcd.setCursor(7,0);
-        lcd.print("START");
-        level3 = true;
-        level2 = false;
-      }
-      
-    }
-    // LEVEL 3 : How many times does the water will boil ?
-    else if(level3)
-    {
-      lcd.setCursor(0,1);
-      lcd.print("In minutes");
-      lcd.setCursor(0,2);
-      lcd.print("Boiling time ?");
-      lcd.setCursor(0,3);
-      lcd.print(heatWort_Time);
-      if(mapX > 600)
-      {
-        heatWort_Time ++;
-      }
-      else if(mapX < 400)
-      {
-        heatWort_Time --;
-      }
-      else if(enter_Value !=1)
-      {
+        lcd.print("STAGE1");
+        lcd.setCursor(1,2);
+        lcd.print("T :");
+        lcd.setCursor(1,8);
+        lcd.print(T1_Value);
+        digitalWrite(heat1_PIN,HIGH);
+        //digitalWrite(heat2_PIN,HIGH);
+        digitalWrite(pump_PIN,LOW);
+        digitalWrite(valve1_PIN,HIGH);
+        if(T1_Value>50 && getTemperature(&T_Value[0], true) == READ_OK)
+        {
+          PHASE = 2;
+        }
+      break;
+    case 2:
+        boucle1 ++;
         lcd.clear();
         lcd.setCursor(7,0);
-        lcd.print("START");
-        level4 = true;
-        level3 = false;
-      }
-      
-    }
-    // LEVEL 4 : How long (after the boiling level starts) will the first hops be added
-    else if(level4)
-    {
-      lcd.setCursor(0,1);
-      lcd.print("Min from boil start");
-      lcd.setCursor(0,2);
-      lcd.print("First houblon ?");
-      lcd.setCursor(0,3);
-      lcd.print(hop1_Time);
-      if(mapX > 600)
-      {
-        hop1_Time ++;
-      }
-      else if(mapX < 400)
-      {
-        hop1_Time --;
-      }
-      else if(enter_Value !=1)
-      {
+        lcd.print("STAGE2");
+        lcd.setCursor(1,2);
+        lcd.print("T :");
+        lcd.setCursor(1,8);
+        lcd.print(T1_Value);
+        digitalWrite(heat1_PIN,LOW);
+        //digitalWrite(heat2_PIN,LOW);
+        digitalWrite(pump_PIN,HIGH);
+        digitalWrite(valve1_PIN,LOW);
+        delay(PHASE2_Time);
+        if(boucle1<4)
+        {
+          PHASE = 1;
+        }
+        else{
+          PHASE = 3;
+        }
+      break;
+    case 3:
         lcd.clear();
         lcd.setCursor(7,0);
-        lcd.print("START");
-        level5 = true;
-        level4 = false;
-      }
-      
+        lcd.print("STAGE3");
+        lcd.setCursor(1,2);
+        lcd.print("T :");
+        lcd.setCursor(1,8);
+        lcd.print(T1_Value);
+        digitalWrite(heat1_PIN,HIGH);
+        //digitalWrite(heat2_PIN,HIGH);
+        digitalWrite(pump_PIN,LOW);
+        digitalWrite(valve1_PIN,HIGH);
+        if(T1_Value>60 && getTemperature(&T_Value[0], true) == READ_OK)
+        {
+          PHASE = 4;
+        }
+      break;
+    case 4:
+        boucle2 ++;
+        lcd.clear();
+        lcd.setCursor(7,0);
+        lcd.print("STAGE4");
+        lcd.setCursor(1,2);
+        lcd.print("T :");
+        lcd.setCursor(1,8);
+        lcd.print(T1_Value);
+        digitalWrite(heat1_PIN,LOW);
+        //digitalWrite(heat2_PIN,LOW);
+        digitalWrite(pump_PIN,HIGH);
+        digitalWrite(valve1_PIN,LOW);
+        delay(PHASE4_Time);
+        if(boucle1<2)
+        {
+          PHASE = 2;
+        }
+        else{
+          PHASE = 5;
+        }
+      break;
+    case 5:
+        lcd.clear();
+        lcd.setCursor(7,0);
+        lcd.print("STAGE5");
+        lcd.setCursor(1,2);
+        lcd.print("T :");
+        lcd.setCursor(1,8);
+        lcd.print(T1_Value);
+        digitalWrite(heat1_PIN,HIGH);
+        //digitalWrite(heat2_PIN,HIGH);
+        digitalWrite(pump_PIN,LOW);
+        digitalWrite(valve1_PIN,HIGH);
+        if(T1_Value>70 && getTemperature(&T_Value[0], true) == READ_OK)
+        {
+          PHASE = 6;
+        }
+      break;
+    case 6:
+        lcd.clear();
+        lcd.setCursor(7,0);
+        lcd.print("STAGE6");
+        lcd.setCursor(1,2);
+        lcd.print("T :");
+        lcd.setCursor(1,8);
+        lcd.print(T1_Value);
+        digitalWrite(heat1_PIN,LOW);
+        //digitalWrite(heat2_PIN,LOW);
+        digitalWrite(pump_PIN,HIGH);
+        digitalWrite(valve1_PIN,LOW);
+        delay(PHASE6_Time);
+          PHASE = 7;
+      break;
+    case 7:
+        lcd.clear();
+        lcd.setCursor(7,0);
+        lcd.print("STAGE7");
+        lcd.setCursor(1,2);
+        lcd.print("T :");
+        lcd.setCursor(1,8);
+        lcd.print(T1_Value);
+        digitalWrite(heat1_PIN,HIGH);
+        //digitalWrite(heat2_PIN,HIGH);
+        digitalWrite(pump_PIN,LOW);
+        digitalWrite(valve1_PIN,HIGH);
+        delay(PHASE7_Time);
+          PHASE = 8;
+      break;
+    case 8:
+        lcd.clear();
+        lcd.setCursor(7,0);
+        lcd.print("STAGE8");
+        lcd.setCursor(1,2);
+        lcd.print("T :");
+        lcd.setCursor(1,8);
+        lcd.print(T1_Value);
+        digitalWrite(heat1_PIN,HIGH);
+        //digitalWrite(heat2_PIN,HIGH);
+        digitalWrite(pump_PIN,LOW);
+        digitalWrite(valve1_PIN,HIGH);
+        //digitalWrite(buz_PIn,HIGH);
+        //delay(5000);
+        //digitalWrite(buz_PIn,LOW);
+        delay(PHASE8_Time);
+          PHASE = 9;
+      break;
+    case 9:
+        lcd.clear();
+        lcd.setCursor(7,0);
+        lcd.print("STAGE9");
+        lcd.setCursor(1,2);
+        lcd.print("T :");
+        lcd.setCursor(1,8);
+        lcd.print(T1_Value);
+        digitalWrite(heat1_PIN,LOW);
+        //digitalWrite(heat2_PIN,LOW);
+        digitalWrite(pump_PIN,LOW);
+        digitalWrite(valve1_PIN,LOW);
+        //digitalWrite(buz_PIn,HIGH);
+      break;
+     default :
+        lcd.clear();
+        lcd.setCursor(7,0);
+        lcd.print("ERROR");
+        digitalWrite(heat1_PIN,LOW);
+        //digitalWrite(heat2_PIN,LOW);
+        digitalWrite(pump_PIN,LOW);
+        digitalWrite(valve1_PIN,LOW);
+        //digitalWrite(buz_PIn,HIGH);
+      break;
     }
-    // LEVEL 4 : How long (after the boiling level starts) will the second hops be added
-    else if(level5)
-    {
-      lcd.setCursor(0,1);
-      lcd.print("Min from boil start");
-      lcd.setCursor(0,2);
-      lcd.print("Second houblon ?");
-      lcd.setCursor(0,3);
-      lcd.print(hop2_Time);
-      if(mapX > 600)
-      {
-        hop2_Time ++;
-      }
-      else if(mapX < 400)
-      {
-        hop2_Time --;
-      }
-      else if(enter_Value !=1)
-      {
-        level6 = true;
-        level5 = false;
-      }
-      
-    }
-    else if(level6)
-    {
-      lcd.setCursor(0,1);
-      lcd.print("Valided ?");
-      lcd.setCursor(0,2);
-      lcd.print("Enter:Y UP:N");
-      if(mapX > 600)
-      {
-        level0=true;
-        level6=false;
-      }
-      else if(enter_Value !=1)
-      {
-        STAGE1 = true;
-        START = false;
-        level0=true;
-        level6=false;
-      }
-      
-    }
-    // Else : ERROR 
-    else
-    {
-    }
-    
-  }
-  // Stage 1 : Heated water. (Kettle, T°1)
-  else if (STAGE1)
-  {
-    lcd.clear();
-    lcd.setCursor(7,0);
-    lcd.print("STAGE1");
-    if(T1_Value<50)
-    {
-      
-    }
-    
-  }
-  // Stage 2 : Run water. (Valve1)
-  else if (STAGE2)
-  {
-    lcd.clear();
-    lcd.setCursor(7,0);
-    lcd.print("STAGE2");
-  }
-  // Stage 3 : Heated the grain. (Hob)
-  else if (STAGE3)
-  {
-    lcd.clear();
-    lcd.setCursor(7,0);
-    lcd.print("STAGE3");
-  }
-  // Stage 4 : Extract the grain. (Motor?)
-  else if (STAGE4)
-  {
-    lcd.clear();
-    lcd.setCursor(7,0);
-    lcd.print("STAGE4");
-  }
-  // Stage 5 : Boil the wort. (Whirlpool with the pump, Hob, T°2)
-  else if (STAGE5)
-  {
-    lcd.clear();
-    lcd.setCursor(7,0);
-    lcd.print("STAGE5");
-  }
-  // Stage 6 : Run the wort for cooling. (Valve2)
-  else if (STAGE6)
-  {
-    lcd.clear();
-    lcd.setCursor(7,0);
-    lcd.print("STAGE6");
-  }
-  // ERROR
-  else
-  {
-    lcd.clear();
-    lcd.setCursor(7,0);
-    lcd.print("Error stage");
-  }
-
 }
 
 // From https://www.carnetdumaker.net/articles/mesurer-une-temperature-avec-un-capteur-1-wire-ds18b20-et-une-carte-arduino-genuino/
